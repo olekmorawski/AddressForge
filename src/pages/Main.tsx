296e74d67cfd96342a212f33a931b5af28fc1427
Aleksander <olek.morawski@gmail.com>
2024-02-18 11:32:14 +0100

    wdwcc

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -41,7 +41,6 @@ function Main() {
   };
 
   return (
-    
     <div className="main">
       <header className="header1">
         <div className="logo_container1">
@@ -57,41 +56,44 @@ function Main() {
         )}
       </header>
       <div className="form-container">
-        <div className="form-box">
-          <h2>Choose address creation purpose</h2>
-          <div className="radio-group">
-            <input type="radio" id="editableAddress" name="addressType" value="editableAddress" onChange={(e) => setAddressCreationPurpose(e.target.value)} defaultChecked />
-            <label htmlFor="editableAddress">Editable address</label>
-          </div>
-          <div className="radio-group">
-            <input type="radio" id="gasReduction" name="addressType" value="gasReduction" onChange={(e) => setAddressCreationPurpose(e.target.value)} />
-            <label htmlFor="gasReduction">Gas reduction</label>
-          </div>
-          <input type="text" placeholder="0x... Enter your desired address" />
-          {addressCreationPurpose === 'gasReduction' && (
-
+      <div className="form-box">
+        <h2>Choose address creation purpose</h2>
+        <div className="radio-group">
+          <input type="radio" id="editableAddress" name="addressType" value="editableAddress" onChange={(e) => setAddressCreationPurpose(e.target.value)} defaultChecked />
+          <label htmlFor="editableAddress">Editable address</label>
+        </div>
+        <div className="radio-group">
+          <input type="radio" id="gasReduction" name="addressType" value="gasReduction" onChange={(e) => setAddressCreationPurpose(e.target.value)} />
+          <label htmlFor="gasReduction">Gas reduction</label>
+        </div>
+        {addressCreationPurpose !== 'gasReduction' && (
           <>
-      <label htmlFor="gasReduction" className="input-label">Choose level of gas reduction</label>
-      <div className="gas-reduction">
-        <button className="gas-reduction-btn" onClick={decrement}>-</button>
-        <input
-          type="text"
-          value={gasReductionLevel}
-          readOnly
-          className="gas-reduction-input"
-        />
-        <button className="gas-reduction-btn" onClick={increment}>+</button>
-      </div>
-      </>
+            <input type="text" placeholder="0x... Enter your desired address" />
+            <div className="radio-group">
+              <input type="radio" id="prefix" name="editablePart" defaultChecked />
+              <label htmlFor="prefix">prefix</label>
+            </div>
+            <div className="radio-group">
+              <input type="radio" id="suffix" name="editablePart" />
+              <label htmlFor="suffix">suffix</label>
+            </div>
+          </>
         )}
-          <div className="radio-group">
-            <input type="radio" id="prefix" name="editablePart" defaultChecked />
-            <label htmlFor="prefix">prefix</label>
-          </div>
-          <div className="radio-group">
-            <input type="radio" id="suffix" name="editablePart" />
-            <label htmlFor="suffix">suffix</label>
-          </div>
+        {addressCreationPurpose === 'gasReduction' && (
+          <>
+            <label htmlFor="gasReduction" className="input-label">Choose level of gas reduction</label>
+            <div className="gas-reduction">
+              <button className="gas-reduction-btn" onClick={decrement}>-</button>
+              <input
+                type="text"
+                value={gasReductionLevel}
+                readOnly
+                className="gas-reduction-input"
+              />
+              <button className="gas-reduction-btn" onClick={increment}>+</button>
+            </div>
+          </>
+        )}
           <input type="text" placeholder="Enter contract deployer address" />
           <div className="infocontainer">
             <div className="inner-div">
